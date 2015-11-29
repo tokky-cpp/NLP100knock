1da5ce77ae56094ebaa149bbd90c4919d07345f4
tokky <tom94826@gmail.com>
2015-11-29 13:12:25 +0900

    変数名変更

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
-str="Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+sentence="Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
-str_l=[]
-str_l=str.split(' ')
+words=[]
+words=sentence.split(' ')
 word_len=[]
 
-for w in str_l:
+for w in words:
     if(w[len(w)-1]=='.' or w[len(w)-1]==','):
 #           print w[0:len(w)-1],len(w)-1
         word_len.append(len(w)-1)
