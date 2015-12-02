faa657c8987b05e14b8b3b9a5db839ab2c9dfea2
tokky <tom94826@gmail.com>
2015-12-02 21:36:36 +0900

    仕切り直し

---
+++ 01.py
@@ -0,0 +1,6 @@
+#coding: UTF-8
+
+str=u'パタトクカシーー'
+
+print str[0::2]
+
---
+++ 02.py
@@ -0,0 +1,11 @@
+#coding: UTF-8
+
+s1=u'パトカー'
+s2=u'タクシー'
+s3=u''
+
+for i in range(len(s1)):
+    s3+=s1[i]+s2[i]
+
+print s3
+
